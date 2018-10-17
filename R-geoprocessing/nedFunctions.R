d0d3a3e4e1343e6f72b52c6d91718d6aadd5f4e9
Stephen Roecker <stephen.roecker@outlook.com>
2018-10-17 16:41:24 -0400

    updating ned and lidar processing code

--- R-geoprocessing/nedFunctions.R
+++ R-geoprocessing/nedFunctions.R
@@ -60,11 +60,11 @@ batch_unzip <- function(zipfile, files, dir_out){
 # Beware, using the cutline option shifts the raster, this can rectified using the -tap option, and shifting the output using raster shift() or gdal_translate -prjwin
 # In order to correct for the half cell shift due to the GTIFF library its necessary to set --config GTIFF_POINT_GEO_IGNORE = TRUE preceding the other gdwarp arguements
 
-batch_crop <- function(input, sso_dsn, office, output, method, crsarg) {
+crop <- function(input, output, folder, sso_dsn, crsarg) {
   
   sso_pol <- read_sf(dsn = sso_dsn, layer = "MLRA_Soil_Survey_Areas_Dec2015")
   sso_pol <- st_transform(sso_pol, crs = crsarg)
-  sso_pol <- subset(sso_pol, NEW_SSAID == office)
+  sso_pol <- subset(sso_pol, NEW_SSAID == folder)
   
   cat("cropping", output, format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "\n")
   
@@ -146,37 +146,36 @@ mosaicNlcdList <- function(mosaiclist, dstpath){
 ### Warp 9d to 10m using -r bilinear
 ################################################################
 ### Notes: ArcGIS doesn't seem to recognize GDAL formatted files. To accommadate this the creation profile options are set to the vanilla GeoTIFF. Also the output projection is spelled out in it's entirey (e.g. "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0") 
-batch_warp <- function(inlist, warplist, reflist, resto, r, s_srs, t_srs, datatype, nodata, co){    
-  for(i in seq(inlist)){
-    cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"WARPING", inlist[i],"\n"))
- te <- c(bbox(raster(reflist[i])))
+warp <- function(input, output, reference, resto, r, s_srs, t_srs, datatype, nodata, co){
+  cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"warping", input,"\n"))
+  
+  te <- c(bbox(raster(reference)))
   gdalwarp(
-    srcfile = inlist[i],
-    dstfile = warplist[i],
+    srcfile = input,
+    dstfile = output,
     s_srs = s_srs,
     t_srs = t_srs,
     te = te,
     r = r,
-    tr = c(resto,resto),
+    tr = c(resto, resto),
     of = "GTiff",
     ot = datatype,
     co = co,
     dstnodata = nodata,
-    overwrite = T,
-    verbose = T
-  )
+    overwrite = TRUE,
+    verbose = TRUE
+    )
   gdaladdo(
-    filename = warplist[i],
+    filename = output,
     r = "nearest",
     levels = c(2, 4, 8, 16),
     clean = TRUE,
     ro = TRUE
-  )
+    )
   gdalinfo(
-    datasetname = warplist[i],
+    datasetname = output,
     stats = TRUE
-  )
-  }
+    )
 }
 
 
@@ -221,96 +220,95 @@ batch_average <- function(demlist, resfrom, resto){
 ### Calculating derivatives
 #################################################################
 # Notes the compression seems to be a problem calculating derivatives, when the file size is greater than say a gigabyte
-batch_DEM <- function(demlist, co){
-  hillshade <- paste0(unlist(strsplit(demlist, ".tif")), "_hillshade.tif")
-  slope <- paste0(unlist(strsplit(demlist, ".tif")), "_slope.tif")
-  slope_temp <- paste0(unlist(strsplit(demlist, ".tif")), "_slope_temp.tif")
-  aspect <- paste0(unlist(strsplit(demlist, ".tif")), "_aspect.tif")
-  aspect_temp <- paste0(unlist(strsplit(demlist, ".tif")), "_aspect_temp.tif")
+dem <- function(input, co){
+  hillshade   <- paste0(gsub(".tif", "", input), "_hillshade.tif")
+  slope       <- paste0(gsub(".tif", "", input), "_slope.tif")
+  slope_temp  <- paste0(gsub(".tif", "", input), "_slope_temp.tif")
+  aspect      <- paste0(gsub(".tif", "", input), "_aspect.tif")
+  aspect_temp <- paste0(gsub(".tif", "", input), "_aspect_temp.tif")
   
-  for(i in seq(demlist)){
-    cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"calculating hillshade for", demlist[i],"\n"))
-    gdaldem(
-      mode = "hillshade",
-      input_dem = demlist[i],
-      output = hillshade[i],
-      of = "GTiff",
-      co = co,
-      verbose = T
-      )    
-    gdaladdo(
-      filename = hillshade[i],
-      r = "nearest",
-      levels = c(2, 4, 8, 16),
-      clean = TRUE,
-      ro = TRUE,
-      )    
-    gdalinfo(
-      datasetname = hillshade[i],
-      stats = TRUE,
-      )
-    
-    cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"calculating slope for", demlist[i],"\n"))
-    gdaldem(
-      mode = "slope",
-      input_dem = demlist[i],
-      output = slope_temp[i],
-      of = "GTiff",
-      p = TRUE,
-      verbose = T
-      )
-    gdal_translate(
-      src_dataset = slope_temp[i],
-      dst_dataset = slope[i],
-      ot = "Byte",
-      co = c("TILED=YES", "COMPRESS=DEFLATE"),
-      overwrite = TRUE,
-      verbose = TRUE
-    )
-    file.remove(slope_temp[i])
-    
-    gdaladdo(
-      filename = slope[i],
-      r = "nearest",
-      levels = c(2, 4, 8, 16),
-      clean = TRUE,
-      ro = TRUE,
+  cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"calculating hillshade for", input,"\n"))
+  
+  gdaldem(
+    mode = "hillshade",
+    input_dem = input,
+    output = hillshade,
+    of = "GTiff",
+    co = co,
+    verbose = TRUE
     )    
-    gdalinfo(
-      datasetname = slope[i],
-      stats = TRUE,
-      hist = TRUE,
-    )
-    cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"calculating aspect for", demlist[i],"\n"))
-    file.remove(aspect_temp[i])
-    gdaldem(
-      mode = "aspect",
-      input_dem = demlist[i],
-      output = aspect_temp[i],
-      of = "GTiff",
-      verbose = T
-    )
-    gdal_translate(
-      src_dataset = aspect_temp[i],
-      dst_dataset = aspect[i],
-      ot = "Int16",
-      co = c("TILED=YES", "COMPRESS=DEFLATE"),
-      overwrite = TRUE,
-      verbose = TRUE
-    )
-    file.remove(aspect_temp[i])
-    
-    gdaladdo(
-      filename = aspect[i],
-      r = "nearest",
-      levels = c(2, 4, 8, 16),
-      clean = TRUE,
-      ro = TRUE,
+  gdaladdo(
+    filename = hillshade,
+    r = "nearest",
+    levels = c(2, 4, 8, 16),
+    clean = TRUE,
+    ro = TRUE
     )    
-    gdalinfo(
-      datasetname = aspect[i],
-      stats = TRUE,
-      hist = TRUE,
+  gdalinfo(
+    datasetname = hillshade,
+    stats = TRUE
+    )
+  
+  cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"calculating slope for", input,"\n"))
+  
+  gdaldem(
+    mode = "slope",
+    input_dem = input,
+    output = slope_temp,
+    of = "GTiff",
+    p = TRUE,
+    verbose = TRUE
+    )
+  gdal_translate(
+    src_dataset = slope_temp,
+    dst_dataset = slope,
+    ot = "Byte",
+    co = c("TILED=YES", "COMPRESS=DEFLATE"),
+    overwrite = TRUE,
+    verbose = TRUE
+    )
+  file.remove(slope_temp)
+  
+  gdaladdo(
+    filename = slope,
+    r = "nearest",
+    levels = c(2, 4, 8, 16),
+    clean = TRUE,
+    ro = TRUE
+    )    
+  gdalinfo(
+    datasetname = slope,
+    stats = TRUE,
+    hist = TRUE
+    )
+  cat(paste(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),"calculating aspect for", input,"\n"))
+  gdaldem(
+    mode = "aspect",
+    input_dem = input,
+    output = aspect_temp,
+    of = "GTiff",
+    verbose = TRUE
+    )
+  gdal_translate(
+    src_dataset = aspect_temp,
+    dst_dataset = aspect,
+    ot = "Int16",
+    co = c("TILED=YES", "COMPRESS=DEFLATE"),
+    overwrite = TRUE,
+    verbose = TRUE
+    )
+  file.remove(aspect_temp)
+    
+  gdaladdo(
+    filename = aspect,
+    r = "nearest",
+    levels = c(2, 4, 8, 16),
+    clean = TRUE,
+    ro = TRUE
+    )    
+  gdalinfo(
+    datasetname = aspect,
+    stats = TRUE,
+    hist = TRUE
     )
   }
-}
